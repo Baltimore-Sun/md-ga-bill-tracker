5a80aba8e33a42dd7da6e68ba0f3bdd5a105a7bf
The Baltimore Sun <131395414+Baltimore-Sun@users.noreply.github.com>
2025-01-10 13:47:54 -0500

    Update script.R

--- script.R
+++ script.R
@@ -2,10 +2,7 @@ library(tidyverse)
 library(janitor)
 library(tidyr)
 
-ga2025 <- read_csv("https://mgaleg.maryland.gov/2025rs/misc/billsmasterlist/BillMasterList.csv") %>% clean_names()
-
-###List bills to track here
-tracker <- ga2025 %>% filter(bill_number == "SB0001" | bill_number == "SB0029")
+tracker <- read_csv("https://mgaleg.maryland.gov/2025rs/misc/billsmasterlist/BillMasterList.csv") %>% clean_names()
 
 tracker <- replace_na(tracker, list(x_file_bill_number="NULL"))
 
@@ -13,7 +10,6 @@ tracker$hyperlinked_billnum <- paste("<a href='https://mgaleg.maryland.gov/mgawe
 
 tracker$hyperlinked_xfile <- paste("<a href='https://mgaleg.maryland.gov/mgawebsite/Legislation/Details/", tracker$x_file_bill_number, "?ys=2025RS' target='_blank'>", tracker$x_file_bill_number, "</a>", sep = "") 
 
-###remove xfile hyperlinks for bills without an xfile
 tracker <- tracker %>%
   mutate(
     dplyr::across(
@@ -22,7 +18,16 @@ tracker <- tracker %>%
     )
   )
 
-###grab columns for table
-tracktab <- select(tracker,hyperlinked_billnum,title,current_status,hyperlinked_xfile)
+###List bills to track here
+gentrack <- tracker %>% filter(bill_number == "SB0001" | bill_number == "SB0029")
+
+###Define Anne Arundel lawmakers here
+aatracker <- tracker %>% filter(grepl("Lehman|Bagnall|Anne Arundel|Bartlett|Chang|Chisholm|Howard|Jones, D|Kipke|Pruski|Rogers|Schmidt|Simmons|Melnyk|Barnes|Beidle|Gile|Henson|Lam|Rosapepe|Simonaire", sponsor, ignore.case=TRUE))
+aatracker <- aatracker %>% arrange(sponsor, bill_number)
+
+###grab columns for tables
+tracktab <- select(gentrack,hyperlinked_billnum,title,current_status,hyperlinked_xfile)
+aatab <- select(aatracker,hyperlinked_billnum,sponsor,broad_subject_name,title,current_status,hyperlinked_xfile)
 
 write_csv(tracktab, "tracktab.csv")
+write_csv(aatab, "aatab.csv")
