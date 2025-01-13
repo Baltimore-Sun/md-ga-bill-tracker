52027d0aa4f5bbd958e0b39e2b0075d3a66c4e41
The Baltimore Sun <131395414+Baltimore-Sun@users.noreply.github.com>
2025-01-13 10:39:20 -0500

    Update script.R
    Add Carroll, Harford, Howard

--- script.R
+++ script.R
@@ -18,16 +18,28 @@ tracker <- tracker %>%
     )
   )
 
-###List bills to track here
 gentrack <- tracker %>% filter(bill_number == "SB0001" | bill_number == "SB0029")
 
-###Define Anne Arundel lawmakers here
-aatracker <- tracker %>% filter(grepl("Lehman|Bagnall|Anne Arundel|Bartlett|Chang|Chisholm|Howard|Jones, D|Kipke|Pruski|Rogers|Schmidt|Simmons|Melnyk|Barnes|Beidle|Gile|Henson|Lam|Rosapepe|Simonaire", sponsor, ignore.case=TRUE))
+aatracker <- tracker %>% filter(grepl("Henson|Nkongolo|Lehman|Bagnall|Anne Arundel|Bartlett|Chang|Chisholm|Howard|Jones, D|Kipke|Pruski|Rogers|Schmidt|Simmons|Melnyk|Barnes|Beidle|Gile|Henson|Lam|Rosapepe|Simonaire", sponsor, ignore.case=TRUE))
 aatracker <- aatracker %>% arrange(sponsor, bill_number)
 
-###grab columns for tables
+carrolltracker <- tracker %>% filter(grepl("Carroll County|Ready|West|Bouchat|Rose|Tomlinson|Stonko", sponsor, ignore.case=TRUE))
+carrolltracker <- carrolltracker %>% arrange(sponsor, bill_number)
+
+hartracker <- tracker %>% filter(grepl("Harford County|Airkan|Johnson, A|Johnson, S| McComas|Griffith|Reilly|Jennings|James|Gallon", sponsor, ignore.case=TRUE))
+hartracker <- hartracker %>% arrange(sponsor, bill_number)
+
+howtracker <- tracker %>% filter(grepl("Howard County|Hester|Lam|Guzzone, G|Wu|Ziegler|Senator Watson|Feldmark|Hill|Atterbeary|Guzzone, P| Terrasa", sponsor, ignore.case=TRUE))
+howtracker <- howtracker %>% arrange(sponsor, bill_number)
+
 tracktab <- select(gentrack,hyperlinked_billnum,title,current_status,hyperlinked_xfile)
 aatab <- select(aatracker,hyperlinked_billnum,sponsor,broad_subject_name,title,current_status,hyperlinked_xfile)
+carrolltab <- select(carrolltracker,hyperlinked_billnum,sponsor,broad_subject_name,title,current_status,hyperlinked_xfile)
+hartab <- select(hartracker,hyperlinked_billnum,sponsor,broad_subject_name,title,current_status,hyperlinked_xfile)
+howtab <- select(howtracker,hyperlinked_billnum,sponsor,broad_subject_name,title,current_status,hyperlinked_xfile)
 
 write_csv(tracktab, "tracktab.csv")
 write_csv(aatab, "aatab.csv")
+write_csv(carrolltab, "carrolltab.csv")
+write_csv(hartab, "hartab.csv")
+write_csv(howtab, "howtab.csv")
